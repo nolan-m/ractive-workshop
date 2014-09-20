4ed19464bdc1104921704ab2e928c6c0a5eaf296
Nolan Miller <student@epicodus.com>
2014-09-20 15:27:25 -0700

    recursively calling partials to display a file structure

---
+++ assets/data/mydata.js
@@ -0,0 +1,356 @@
+var mydata = [
+  {
+    "_id": "541dd28e0d02c1ad7ed19390",
+    "index": 0,
+    "guid": "6cda01b9-a014-4f77-af78-17a173caee06",
+    "isActive": true,
+    "balance": "$1,153.50",
+    "picture": "http://placehold.it/32x32",
+    "age": 22,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Anthony",
+      "last": "Lamb"
+    },
+    "company": "NETILITY",
+    "email": "anthony.lamb@netility.ca",
+    "phone": "+1 (851) 544-2324",
+    "address": "673 Bryant Street, Aurora, West Virginia, 1944",
+    "about": "Nisi incididunt aute velit Lorem aliqua velit. Ea ut cillum id dolore id reprehenderit dolore. Aliquip magna culpa minim labore ut adipisicing elit eu nisi velit reprehenderit exercitation aliqua.\r\n",
+    "registered": "Thursday, March 6, 2014 8:50 AM",
+    "latitude": -27.863955,
+    "longitude": 29.027957,
+    "tags": [
+      "officia",
+      "veniam",
+      "irure",
+      "consectetur",
+      "adipisicing",
+      "laboris",
+      "do"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Kane Castro"
+      },
+      {
+        "id": 1,
+        "name": "Keller Singleton"
+      },
+      {
+        "id": 2,
+        "name": "Larsen Richard"
+      }
+    ],
+    "greeting": "Hello, Anthony! You have 10 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "541dd28ea4d0ffc17063cd38",
+    "index": 1,
+    "guid": "ac463c99-c289-42b0-b541-f42b763b4d46",
+    "isActive": false,
+    "balance": "$2,920.57",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": {
+      "first": "James",
+      "last": "Molina"
+    },
+    "company": "COMCUBINE",
+    "email": "james.molina@comcubine.info",
+    "phone": "+1 (939) 569-3419",
+    "address": "834 Buffalo Avenue, Dotsero, Wyoming, 6428",
+    "about": "Veniam Lorem ad deserunt id do cillum pariatur laboris elit. Anim ad culpa adipisicing commodo sit excepteur fugiat magna aute do. Aliquip enim non eiusmod cupidatat et irure reprehenderit aliquip sunt enim excepteur officia excepteur. Pariatur consequat elit amet ex nisi Lorem non consectetur amet ad officia ea dolore aliqua. Dolore occaecat sit ad enim. Est dolor occaecat voluptate in velit duis minim minim ea eiusmod quis sit. Sunt non minim deserunt do commodo fugiat Lorem ad dolor qui.\r\n",
+    "registered": "Monday, July 21, 2014 9:20 AM",
+    "latitude": 42.039431,
+    "longitude": 74.366707,
+    "tags": [
+      "deserunt",
+      "dolore",
+      "et",
+      "nisi",
+      "duis",
+      "magna",
+      "magna"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Tonia Vance"
+      },
+      {
+        "id": 1,
+        "name": "Huffman Robles"
+      },
+      {
+        "id": 2,
+        "name": "Hurst Chaney"
+      }
+    ],
+    "greeting": "Hello, James! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "541dd28ec90c402677db5a77",
+    "index": 2,
+    "guid": "38d9246a-c580-450a-b503-5e6ef2996600",
+    "isActive": true,
+    "balance": "$3,776.23",
+    "picture": "http://placehold.it/32x32",
+    "age": 22,
+    "eyeColor": "green",
+    "name": {
+      "first": "Stokes",
+      "last": "Cobb"
+    },
+    "company": "EVEREST",
+    "email": "stokes.cobb@everest.me",
+    "phone": "+1 (859) 545-3200",
+    "address": "357 Dahlgreen Place, Belvoir, Palau, 5661",
+    "about": "Fugiat id ex commodo aliquip officia commodo. Quis reprehenderit ipsum irure sint adipisicing eiusmod aute dolor. Proident deserunt reprehenderit dolor qui mollit eiusmod minim deserunt dolor. Aliqua irure in elit id consequat velit ea est non magna reprehenderit. Culpa commodo minim ad veniam nulla culpa ex sint est cupidatat. Voluptate incididunt proident nisi deserunt sint eu. Ex do ut incididunt deserunt dolor eiusmod sint incididunt velit quis reprehenderit excepteur.\r\n",
+    "registered": "Saturday, June 28, 2014 4:10 AM",
+    "latitude": 10.484755,
+    "longitude": -64.699121,
+    "tags": [
+      "sint",
+      "dolore",
+      "non",
+      "esse",
+      "id",
+      "aute",
+      "dolore"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Petty Macias"
+      },
+      {
+        "id": 1,
+        "name": "Alyce Walker"
+      },
+      {
+        "id": 2,
+        "name": "Mariana Castillo"
+      }
+    ],
+    "greeting": "Hello, Stokes! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "541dd28ec69a02202b09b9ae",
+    "index": 3,
+    "guid": "aa05588a-b7e1-4abe-bb72-48f2ce35b532",
+    "isActive": false,
+    "balance": "$3,919.75",
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Valdez",
+      "last": "Little"
+    },
+    "company": "EXOSWITCH",
+    "email": "valdez.little@exoswitch.biz",
+    "phone": "+1 (947) 544-3584",
+    "address": "431 Malbone Street, Sabillasville, Mississippi, 6692",
+    "about": "Anim eiusmod dolor magna proident occaecat in tempor sint dolore do tempor ipsum. Adipisicing eiusmod dolore velit commodo esse excepteur eiusmod incididunt officia nisi sit culpa esse. Duis minim Lorem laboris adipisicing tempor cupidatat excepteur consequat voluptate cillum excepteur mollit. Anim do culpa mollit sit fugiat aliquip enim sint. Ut nisi dolore ipsum aliqua incididunt ipsum aliquip proident incididunt in non ad. Nostrud dolor non eiusmod deserunt cupidatat aliquip id ea nulla in aliqua anim eiusmod laborum.\r\n",
+    "registered": "Thursday, January 30, 2014 3:52 AM",
+    "latitude": 28.976326,
+    "longitude": 52.810322,
+    "tags": [
+      "est",
+      "dolore",
+      "minim",
+      "mollit",
+      "officia",
+      "eu",
+      "do"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Rosie Forbes"
+      },
+      {
+        "id": 1,
+        "name": "Fry Haney"
+      },
+      {
+        "id": 2,
+        "name": "Shepherd Patterson"
+      }
+    ],
+    "greeting": "Hello, Valdez! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "541dd28e7d0328caa1e1d041",
+    "index": 4,
+    "guid": "482b00c2-a23b-4f5a-a2e9-d7a85e841d35",
+    "isActive": false,
+    "balance": "$2,977.74",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "blue",
+    "name": {
+      "first": "Theresa",
+      "last": "Medina"
+    },
+    "company": "ZENSOR",
+    "email": "theresa.medina@zensor.biz",
+    "phone": "+1 (868) 431-3765",
+    "address": "482 Hendrickson Street, Coalmont, Delaware, 2266",
+    "about": "Proident ea labore aute nulla magna labore. Id ullamco irure nulla esse voluptate adipisicing. Quis ut laboris ea cupidatat adipisicing velit ut cillum.\r\n",
+    "registered": "Friday, May 30, 2014 2:32 PM",
+    "latitude": 37.56271,
+    "longitude": -119.255179,
+    "tags": [
+      "ut",
+      "eiusmod",
+      "ex",
+      "et",
+      "tempor",
+      "duis",
+      "irure"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Elisabeth Copeland"
+      },
+      {
+        "id": 1,
+        "name": "Cervantes Nunez"
+      },
+      {
+        "id": 2,
+        "name": "Curtis Williamson"
+      }
+    ],
+    "greeting": "Hello, Theresa! You have 9 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "541dd28e94eefcb63e080716",
+    "index": 5,
+    "guid": "30a81abc-4368-4068-826b-0d4498d6103d",
+    "isActive": false,
+    "balance": "$3,690.99",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Chasity",
+      "last": "Miles"
+    },
+    "company": "SCENTY",
+    "email": "chasity.miles@scenty.net",
+    "phone": "+1 (940) 463-3307",
+    "address": "758 Campus Place, Elwood, Hawaii, 3356",
+    "about": "Labore qui culpa sint commodo Lorem dolor amet aute duis. Officia velit non anim mollit Lorem. Velit ex esse aute in amet sint commodo et sint commodo ad voluptate laboris non.\r\n",
+    "registered": "Sunday, May 18, 2014 8:20 AM",
+    "latitude": 88.689786,
+    "longitude": 21.706008,
+    "tags": [
+      "deserunt",
+      "sint",
+      "aliquip",
+      "sunt",
+      "occaecat",
+      "enim",
+      "aliqua"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Emma Chase"
+      },
+      {
+        "id": 1,
+        "name": "Katy Duncan"
+      },
+      {
+        "id": 2,
+        "name": "Waller Delaney"
+      }
+    ],
+    "greeting": "Hello, Chasity! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
